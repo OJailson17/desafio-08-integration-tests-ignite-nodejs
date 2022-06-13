0c1826b6a8dc9323ea2f1ed1b1c46e956e5a80d3
OJailson17 <JaylsonO17@gmail.com>
2022-06-13 01:39:37 -0300

    add tests to user profile route

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import { v4 as uuidV4 } from "uuid";
 import { Connection, createConnection } from "typeorm";
 import { app } from "../../../../app";
+import { ShowUserProfileError } from "./ShowUserProfileError";
 
 let connection: Connection;
 describe("Show user profile", () => {
@@ -41,5 +42,13 @@ describe("Show user profile", () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("id");
+  });
+
+  it("should not be able to make a statement if user id does not exist", async () => {
+    await expect(
+      request(app).get(`/api/v1/profile`).set({
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiOGRiYmZhN2MtMjJhZS00YmY2LWIxY2UtYmMwNGQ3OTU2ZTI1IiwibmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkByZW50YWx4LmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JGZZMkcvd1NYNENVUWZzeG1PLmdkUGVTemdpbHN2MGY2OGVRQTVJM053bWFVRU1GdUw1c3ZpIiwiY3JlYXRlZF9hdCI6IjIwMjItMDYtMDVUMDM6MjE6NDMuMTM4WiIsInVwZGF0ZWRfYXQiOiIyMDIyLTA2LTA1VDAzOjIxOjQzLjEzOFoifSwiaWF0IjoxNjU0Mzk5MzAzLCJleHAiOjE2NTQ0ODU3MDMsInN1YiI6IjhkYmJmYTdjLTIyYWUtNGJmNi1iMWNlLWJjMDRkNzk1NmUyNSJ9.HnJiMtl1R5oiPkfydP76NCkBGKQW0485kVz-BPErzd8`,
+      })
+    ).resolves.toHaveProperty("status", new ShowUserProfileError().statusCode);
   });
 });
