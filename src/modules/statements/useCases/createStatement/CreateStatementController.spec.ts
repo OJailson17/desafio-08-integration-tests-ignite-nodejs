5343fe3ca7693e3375cb3b4ffd94a5b9874f012a
OJailson17 <JaylsonO17@gmail.com>
2022-06-13 01:37:56 -0300

    add tests to create statement route

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import { v4 as uuidV4 } from "uuid";
 import { Connection, createConnection } from "typeorm";
 import { app } from "../../../../app";
+import { CreateStatementError } from "./CreateStatementError";
 
 let connection: Connection;
 describe("Create Statement", () => {
@@ -36,7 +37,7 @@ describe("Create Statement", () => {
     const response = await request(app)
       .post("/api/v1/statements/deposit")
       .send({
-        amount: 200,
+        amount: 100,
         description: "new deposit",
       })
       .set({
@@ -58,7 +59,7 @@ describe("Create Statement", () => {
     await request(app)
       .post("/api/v1/statements/deposit")
       .send({
-        amount: 400,
+        amount: 200,
         description: "new deposit",
       })
       .set({
@@ -68,7 +69,7 @@ describe("Create Statement", () => {
     const response = await request(app)
       .post("/api/v1/statements/withdraw")
       .send({
-        amount: 100,
+        amount: 50,
         description: "new deposit",
       })
       .set({
@@ -77,5 +78,46 @@ describe("Create Statement", () => {
 
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
+  });
+
+  it("should not be able to make a new deposit if user id does not exist", async () => {
+    await expect(
+      request(app)
+        .post(`/api/v1/statements/deposit`)
+        .send({
+          amount: 200,
+          description: "new deposit",
+        })
+        .set({
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiOGRiYmZhN2MtMjJhZS00YmY2LWIxY2UtYmMwNGQ3OTU2ZTI1IiwibmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkByZW50YWx4LmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JGZZMkcvd1NYNENVUWZzeG1PLmdkUGVTemdpbHN2MGY2OGVRQTVJM053bWFVRU1GdUw1c3ZpIiwiY3JlYXRlZF9hdCI6IjIwMjItMDYtMDVUMDM6MjE6NDMuMTM4WiIsInVwZGF0ZWRfYXQiOiIyMDIyLTA2LTA1VDAzOjIxOjQzLjEzOFoifSwiaWF0IjoxNjU0Mzk5MzAzLCJleHAiOjE2NTQ0ODU3MDMsInN1YiI6IjhkYmJmYTdjLTIyYWUtNGJmNi1iMWNlLWJjMDRkNzk1NmUyNSJ9.HnJiMtl1R5oiPkfydP76NCkBGKQW0485kVz-BPErzd8`,
+        })
+    ).resolves.toHaveProperty(
+      "text",
+      new CreateStatementError.UserNotFound().statusCode
+    );
+  });
+
+  it("should not be able to make a withdraw if funds are insuficient", async () => {
+    const responseAuth = await request(app).post("/api/v1/sessions").send({
+      email: "admin@rentalx.com",
+      password: "admin",
+    });
+
+    const { token } = responseAuth.body;
+
+    await expect(
+      request(app)
+        .post("/api/v1/statements/withdraw")
+        .send({
+          amount: 999,
+          description: "new withdraw",
+        })
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+    ).resolves.toHaveProperty(
+      "status",
+      new CreateStatementError.InsufficientFunds().statusCode
+    );
   });
 });
